28a615e8fe7027fdf79f2f6d8ec54af8f05edcca
tevitanpro <estebancarreno23@gmail.com>
2026-08-27 09:07:40 -0500

    Update main.py

--- main.py
+++ main.py
@@ -148,6 +148,7 @@ def _send_email(to_email: str, subject: str, body: str):
         headers={
             "Authorization": f"Bearer {api_key}",
             "Content-Type": "application/json"
+             "User-Agent": "API-Master-Pro/8.8"
         },
         method="POST"
     )
